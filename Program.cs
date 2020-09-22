5619c3f7476abc181cd98b1b28be1a7ed502221f
Vadim <vadimmm.m@ya.ru>
2020-09-22 14:07:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Pr3S2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamReader sr = new StreamReader(@"boot.ini");
+      StreamWriter sw = new StreamWriter(@"boot-utf7.txt", false, Encoding.UTF7);
+      sw.WriteLine(sr.ReadToEnd());
+      sw.Close();
+      sr.Close();
+      Console.WriteLine("Converting completed");
+    }
+  }
+}
